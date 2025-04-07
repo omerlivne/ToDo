ea345f2f080d3c6c2bfadb9d6c5c9f80579aa12d
Omer Livne <omerlivne15@gmail.com>
2025-04-07 19:13:46 +0300

    delete tests

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 # app.py
-import os
-
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))))
+    app.run(debug=True)

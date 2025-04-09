19bb85b9644d6edfa0327026abd3fda271dad440
Omer Livne <omerlivne15@gmail.com>
2025-04-09 18:56:25 +0300

    delete tests

--- app/models/task.py
+++ app/models/task.py
@@ -4,7 +4,7 @@ from app.extensions import db
 class Task(db.Model):
     __tablename__ = "tasks"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.Text)
     status = db.Column(db.String(20), default="Pending", nullable=False)
     due_date = db.Column(db.DateTime)
